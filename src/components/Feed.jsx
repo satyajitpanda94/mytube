ab4799a8654c384f8b9a7fd00251c87b160a04c3
satyajitpanda94 <saatjit94@gmail.com>
2023-11-29 17:43:23 +0530

    Add. Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL, options } from '../utils/axios'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+import './css/Feed.css'
 
 export default function Feed() {
-  return (
-    <div>Feed</div>
-  )
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/search?q=${'bbc'}&type=date`, options)
+            .then(({ data }) => setItems(data.items))
+    }, [])
+    console.log(items)
+
+    return (
+        <div className='feed-container'>
+            {
+                items.length !== 0 && items.map((item, idx) => (
+                    <div key={idx}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && <ChannelCard channel={item} />}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
---
+++ src/utils/axios.js
@@ -0,0 +1,14 @@
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
+export const options = {
+    method: 'GET',
+    params: {
+        part: 'snippet',
+        maxResults: '2',
+    },
+    headers: {
+        'X-RapidAPI-Key': '29ca1379dbmshb2445b4b448df58p1b8eeejsnef8ce7772788',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};

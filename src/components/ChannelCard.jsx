9c1e7da23e8ffb8fc9db699812ea9181b68ece18
satyajitpanda94 <saatjit94@gmail.com>
2023-11-29 23:30:51 +0530

    Add. ChannelCard component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './css/ChannelCard.css'
 
-export default function ChannelCard() {
-  return (
-    <div>ChannelCard</div>
-  )
+export default function ChannelCard({ channel }) {
+    return (
+        <div className='channelcard-container'>
+            <div className="channel-thumbnail-container">
+                <img
+                    src={channel.snippet.thumbnails.high.url || channel.snippet.thumbnails.default.url}
+                    alt=""
+                    className='channel-thumbnail'
+                />
+            </div>
+            <div className="channelcard-buttom">
+                <h3>{channel.snippet.channelTitle}</h3>
+            </div>
+        </div>
+    )
 }

d5e49759e1ef740378ed0f0dcad74cb4e95f3c4a
satyajitpanda94 <saatjit94@gmail.com>
2024-03-31 23:06:40 +0530

    Add. enviornment variables

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,10 +5,10 @@ export const options = {
     method: 'GET',
     params: {
         part: 'snippet',
-        maxResults: '5',
+        maxResults: process.env.REACT_API_MAX_RESULTS,
     },
     headers: {
-        'X-RapidAPI-Key': '29ca1379dbmshb2445b4b448df58p1b8eeejsnef8ce7772788',
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
